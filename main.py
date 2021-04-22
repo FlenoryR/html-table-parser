3079118e7d3a8a50c2b0e05fec67e4549528532b
Artyom Tarasov <flenoryby@gmail.com>
2021-04-22 14:23:37 +0300

    [*] Implementation of parsing HTML tables using Pandas

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
+import pandas as pd
+import json
+import io
+
+application = Flask(__name__)
+CORS(application)
+
+
+@application.route('/api/action/scraping/table', methods=['POST'])
+def table_scraping():
+    output = io.StringIO()
+    data = json.loads(request.data)
+
+    pd.read_html(data['data'])[0].to_csv(output)
+
+    return make_response(
+        jsonify(
+            {
+                'output': output.getvalue()
+            }
+        ),
+        200
+    )
+
+
+if __name__ == '__main__':
+    application.run(
+        host='127.0.0.1',
+        port=3000,
+        debug=True
+    )
